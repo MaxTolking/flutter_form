d0f616c6bedd98339d23fde47687f1816f50022a
Maksim Koblov <maksim.koblov@tularegion.ru>
2022-11-22 15:53:13 +0300

    add SnackBar and AlertDialog

--- lib/widgets/pages/register_form_page.dart
+++ lib/widgets/pages/register_form_page.dart
@@ -1,12 +1,12 @@
-// ignore_for_file: avoid_print, library_private_types_in_public_api
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
 import 'package:flutter_form/widgets/comment_form.dart';
 import 'package:flutter_form/widgets/country_form.dart';
 import 'package:flutter_form/widgets/email_form.dart';
 import 'package:flutter_form/widgets/phone_form.dart';
-
-import '../surname_form.dart';
-import '../name_form.dart';
+import 'package:flutter_form/widgets/surname_form.dart';
+import 'package:flutter_form/widgets/name_form.dart';
 
 class RegisterForm extends StatefulWidget {
   const RegisterForm({super.key});
@@ -17,6 +17,7 @@ class RegisterForm extends StatefulWidget {
 
 class _RegisterFormPageState extends State<RegisterForm> {
   final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   final _nameController = TextEditingController();
   final _surnameController = TextEditingController();
@@ -40,6 +41,7 @@ class _RegisterFormPageState extends State<RegisterForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: const Text('Форма Регистрации'),
         centerTitle: true,
@@ -79,16 +81,69 @@ class _RegisterFormPageState extends State<RegisterForm> {
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      print('Форма заполнена правильно!');
-      print('Имя: ${_nameController.text}');
-      print('Фамилия: ${_surnameController.text}');
-      print('Телефон: ${_phoneController.text}');
-      print('Email: ${_emailController.text}');
-      print('Страна: $_countries');
-      print('Комментарий: $_commentController');
+      _showDialog(name: _nameController.text);
+      if (kDebugMode) {
+        print('Форма заполнена правильно!');
+        print('Фамилия: ${_surnameController.text}');
+        print('Телефон: ${_phoneController.text}');
+        print('Email: ${_emailController.text}');
+        print('Страна: $_countries');
+        print('Комментарий: $_commentController');
+      }
     } else {
-      print(
-          'Данные в форме введены неправильно! Пожалуйста, исправьте и повторите попытку');
+      _showMessage(
+          message:
+              'Данные в форме введены неправильно! Пожалуйста, исправьте данные и повторите попытку.');
     }
   }
+
+  void _showMessage({message}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        backgroundColor: Colors.red,
+        content: Text(
+          message!,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void _showDialog({String? name, context}) {
+  showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          icon: const Icon(
+            Icons.check_circle,
+            color: Colors.green,
+          ),
+          title: const Text('Регистрация прошла успешно!',
+              style: TextStyle(color: Colors.blueGrey)),
+          content: Text(
+            '$name теперь зарегистрирован',
+            style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text(
+                'Отлично!',
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ],
+        );
+      });
 }
